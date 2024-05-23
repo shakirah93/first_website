2bf1ea7751228096f7bb77ef6b4a312d390c7e80
oyinkan <skusman@student.lautech.edu.ng>
2024-05-23 18:33:42 +0100

    added more JavaScript code

--- index.js
+++ index.js
@@ -73,6 +73,11 @@ newConversation("Varlene", "HTML");
 // function expression
 const multiply = function (num) {
   return num * num;
+  /* working with multiple lines of code inside the return statement
+  return (
+    num * num
+  );
+  */
 };
 
 console.log(multiply(2));
@@ -96,11 +101,10 @@ age = 30;
 console.log(age);
 
 // an immediately-invoked function expression
-(function() {
+(function () {
   // code to execute
   console.log("running now");
-}());
-
+})();
 
 // function expression
 
@@ -115,6 +119,133 @@ const multiply = function (num) {
 // an arrow function
 const multiply2 = (num) => {
   return num * num;
-}
+};
 
-const multiply3 = num => num * num;
+const multiply3 = (num) => num * num;
+
+const person = {
+  firstName: "Sakirat",
+  lastName: "Usman",
+  likes: "Pizza",
+};
+
+// dot notation
+console.log(person.firstName);
+
+// bracket notation
+console.log(person["lastName"]);
+
+person.likes = "Shawarma";
+
+console.log(person);
+
+const personA = {
+  firstName: "Monique",
+  likes: "pizza",
+};
+
+const personB = personA;
+
+console.log("Before");
+console.log(personA.firstName);
+console.log(personB.firstName);
+
+personA.firstName = "Stacy";
+console.log("After");
+console.log(personA.firstName);
+console.log(personB.firstName);
+
+// Numbers can be integer or decimal values
+const pi = 3.14;
+
+console.log("The value of pi: " + pi);
+
+// boolean values
+const codherIsAmazing = true;
+const weatherIsGreat = false;
+
+console.log("Is codher amazing? " + codherIsAmazing);
+console.log("Is the weather great? " + weatherIsGreat);
+
+// basic maths operators
+const x = 6;
+const y = 3;
+const add = x + y;
+
+console.log("Addition of x and y is: " + add);
+
+const subtraction = x - y;
+console.log("Subtraction of x and y is: " + subtraction);
+
+const multiplication = x * y;
+console.log("Multiplication of x and y is: " + multiplication);
+
+const division = x / y;
+console.log("Division of x and y is: " + division);
+
+const modulus = x % y;
+console.log("Remainder of x and y is: " + modulus);
+
+const exponentiation = x ** y;
+console.log("Exponentiation of x and y is: " + exponentiation);
+
+let i = 7;
+let j = 8;
+let k = 9;
+let l = 10;
+
+// increment i first by 1, then assign the incremented value of i to preIncrement variable.
+const preIncrement = ++i; // 8
+
+// assign the current value of j to the postIncrement variable, then increment j afterwards.
+// which means, the next time you use j, j will be 9
+const postIncrement = j++; // 8
+
+const preDecrement = --k; // 8
+
+const postDecrement = l--; // 10
+
+console.log("Pre increment of i : " + preIncrement);
+console.log("Post increment of j : " + postIncrement);
+console.log("Pre decrement of k : " + preDecrement);
+console.log("Post decrement of l : " + postDecrement);
+
+// "===" strict equality operator
+
+const apples = "apples";
+const oranges = "oranges";
+
+const isEqual = apples !== oranges;
+
+console.log("Are apples and oranges the same? " + isEqual);
+
+// "==" normal equality operator (it will only if the values are equal)
+const equality = 2 == "2";
+console.log(equality);
+
+// check the type of the value, and also check if they have the same values
+const strictEquality = 2 === "2";
+console.log(strictEquality);
+
+const volunteers = 20;
+const students = 24;
+const pizzas = 25;
+
+const moreStudents = students > volunteers; // true
+
+const lessStudents = students < pizzas; // true
+
+console.log("Are there more students than volunteers? " + moreStudents);
+console.log("Are there fewer students than pizzas? " + lessStudents);
+
+const enoughPizzas = volunteers + students < pizzas;
+
+console.log("Are there enough pizzas for everybody? " + enoughPizzas);
+
+const myAge = 30;
+const minimumDrivingAge = 16;
+
+const canDrive = myAge >= minimumDrivingAge;
+
+console.log("Can I drive? " + canDrive);
+
